fe76aedb3dff51c4b7ecbfef293518033b11db4c
Maxim Abavi <afghan1363@ya.ru>
2023-08-12 01:34:03 +0500

    add tests fo Item()

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
+import pytest
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+@pytest.fixture
+def item():
+    return Item("Notebook", 60000, 10)
+
+def test_item_calculate_total_price(item):
+    assert item.calculate_total_price() == 600000
+
+def test_item_apply_discount(item):
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.pay_rate * 60000 == item.price
