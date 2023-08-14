787a2207b0ced034b152dfd26cbbca32d0c8c6da
Maxim Abavi <afghan1363@ya.ru>
2023-08-14 23:48:15 +0500

    test name_setter

--- tests/test_item.py
+++ tests/test_item.py
@@ -4,7 +4,7 @@ from src.item import Item
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 @pytest.fixture
 def item():
-    return Item("Notebook", 60000, 10)
+    return Item("Смартфон", 60000, 10)
 
 def test_item_calculate_total_price(item):
     assert item.calculate_total_price() == 600000
@@ -13,3 +13,8 @@ def test_item_apply_discount(item):
     item.pay_rate = 0.8
     item.apply_discount()
     assert item.pay_rate * 60000 == item.price
+
+def test_name(item):
+    item.name = "СуперСмартфон"
+    assert item.name == "СуперСмарт"
+
