901c36db07c4a1b4288fd37d7d2b633392f96696
Maxim Abavi <afghan1363@ya.ru>
2023-08-14 23:56:07 +0500

    homework

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os.path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_str):
+        if len(name_str) > 10:
+            self.__name = name_str[:10]
+        else:
+            self.__name = name_str
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +46,17 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        path_to_csv = os.path.join("..", "src", "items.csv")
+        with open(path_to_csv, encoding="cp1251") as csv_data:
+            reader = csv.DictReader(csv_data, delimiter=",")
+            for row in reader:
+                item = cls(row["name"], row["price"], row["quantity"])
+
+    @staticmethod
+    def string_to_number(str_int):
+        return int(float(str_int))
