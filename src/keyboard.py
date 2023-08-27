7854872a8dc2d1be7a4f57ad432f0d6cf4aa9dab
Maxim Abavi <afghan1363@ya.ru>
2023-08-27 22:58:54 +0500

    исправил код, доделал задание

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,23 +2,24 @@ from src.item import Item
 
 
 class Mixin:
-    language = "EN"
-    
+    """Класс для реализации выбора раскладки для товара 'клавиатура'"""
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
-        self.language = Mixin.language
-        Mixin.language = self.language
+        self.__language = "EN"
 
     def change_lang(self):
         if self.language == "EN":
-            self.language = "RU"
+            self.__language = "RU"
         elif self.language == "RU":
-            self.language = "EN"
+            self.__language = "EN"
         return self
+
+    @property
+    def language(self):
+        return self.__language
 
 
 class Keyboard(Mixin, Item):
-
+    """Класс товара 'клавиатура' с выбором раскладки"""
     def __init__(self, name: str, price: float, quantity: int):
         super().__init__(name, price, quantity)
-# self.language = "EN"
