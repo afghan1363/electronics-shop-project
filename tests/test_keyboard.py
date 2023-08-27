1511525a6c7e6ae1c4141cc5b9a0216966fae0ac
Maxim Abavi <afghan1363@ya.ru>
2023-08-27 22:57:18 +0500

    tests keyboard

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -7,16 +7,21 @@ from src.keyboard import Keyboard
 def k_board():
     return Keyboard('UltraSuperKlava', 30000, 50)
 
-@pytest.fixture
-def item():
-    return Item("Смартфон", 60000, 10)
+
+def test_keyboard_name(k_board):
+    assert k_board.name == "UltraSuperKlava"
 
 
 def test_keyboard_language(k_board):
     assert k_board.language == "EN"
+    with pytest.raises(AttributeError):
+        k_board.language = 'CH'
 
 
 def test_change_lang(k_board):
     k_board.change_lang()
     assert str(k_board.language) == "RU"
-
+    k_board.change_lang()
+    assert str(k_board.language) == "EN"
+    k_board.change_lang().change_lang()
+    assert str(k_board.language) == "EN"
